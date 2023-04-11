6cfa836498e5c261b64acaabc7e8a86ef39be76c
hasan <hasansennn82@gmail.com>
2023-04-11 05:24:23 +0300

    11.04

---
+++ src/test/java/tests/C06_Post_ResponseBodyTesti.java
@@ -0,0 +1,31 @@
+package tests;
+
+import org.junit.Test;
+
+public class C06_Post_ResponseBodyTesti {
+
+    /*  https://jsonplaceholder.typicode.com/posts
+         url’ine asagidaki body ile bir POST request gonderdigimizde
+        {
+        "title":"API",
+        "body":"API ogrenmek ne guzel",
+        "userId":10,
+        }
+        donen Response’un,
+        status code’unun 201,
+        ve content type’inin application/json
+        ve Response Body'sindeki,
+        "title"'in "API" oldugunu
+        "userId" degerinin 100'den kucuk oldugunu
+        "body" nin "API" kelimesi icerdigini
+        test edin.
+      */
+
+    @Test
+    public void post01() {
+        // 1 - Request icin gerekli url ve Post hazirla
+
+
+
+    }
+}
