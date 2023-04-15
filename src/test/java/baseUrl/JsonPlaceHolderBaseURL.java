ed66e99777a32c6cdcf5fd79c106246c8bfed5ad
hasan <hasansennn82@gmail.com>
2023-04-15 13:09:50 +0300

    15.04.V1

---
+++ src/test/java/baseUrl/JsonPlaceHolderBaseURL.java
@@ -0,0 +1,24 @@
+package baseUrl;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
+import org.junit.runner.Request;
+
+public class JsonPlaceHolderBaseURL {
+
+    // https://jsonplaceholder.typicode.com
+
+   protected RequestSpecification specJsonPlace;
+
+    @Before
+    public void setUp(){
+
+      specJsonPlace =new RequestSpecBuilder()
+                            .setBaseUri("https://jsonplaceholder.typicode.com")
+                            .build();
+
+
+
+    }
+}
